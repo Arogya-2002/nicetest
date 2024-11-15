0c18f86bd479756f386d5fb561af0d7db92ae119
Arogya-2002 <arogyavamshi2002@gmail.com>
2024-11-15 15:14:46 +0530

    Delete Nice directory

--- Nice/src/main/java/com/project/service/Admin_Service.java
+++
@@ -1,9 +0,0 @@
-package com.project.service;
-
-
-public interface Admin_Service {
-
-	public  String getRoomBill(Long cid);
-	public String getFoodBill(Long cid);
-	public Double getTotalBill(Long cid);
-}
